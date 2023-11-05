312d690583f9ec7725601845836709c6c326b3e5
harbey127 <harbey127@gmail.com>
2023-11-05 19:06:05 +0200

    Edit

--- lib/res/assets_res.dart
+++ lib/res/assets_res.dart
@@ -7,6 +7,8 @@ class AssetsRes {
   static const String PLUGIN_VERSION = '1.0.0+1';
   static const String ERROR = 'assets/images/error.gif';
   static const String LOADING = 'assets/images/loading.gif';
+  static const String ON_BOARDING1 = 'assets/images/on_boarding1.png';
+  static const String ON_BOARDING3 = 'assets/images/on_boarding3.png';
   static const String PLATE_BACKGROUND = 'assets/images/plate_background.png';
   static const String SUCCESS = 'assets/images/success.gif';
   static const String LOGO = 'assets/logo/logo.png';
