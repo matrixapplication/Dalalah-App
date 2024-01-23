ecc799ed9ce28ec8770d6e0f0b159a9fa1f6d7b2
harbey127 <harbey127@gmail.com>
2024-01-23 11:58:15 +0200

    Review Notes in jira

--- lib/src/auth/data/data_sources/forgot_password_datasource.g.dart
+++ lib/src/auth/data/data_sources/forgot_password_datasource.g.dart
@@ -13,7 +13,7 @@ class _ForgotPasswordDataSource implements ForgotPasswordDataSource {
     this._dio, {
     this.baseUrl,
   }) {
-    baseUrl ??= 'http://dalala.matrix-clouds.com/api';
+    baseUrl ??= 'http://dalalah.co/api';
   }
 
   final Dio _dio;
--- lib/src/sell_car/data/data_sources/sell_car_datasource.g.dart
+++ lib/src/sell_car/data/data_sources/sell_car_datasource.g.dart
@@ -13,7 +13,7 @@ class _SellCarDatasource implements SellCarDatasource {
     this._dio, {
     this.baseUrl,
   }) {
-    baseUrl ??= 'http://dalala.matrix-clouds.com/api';
+    baseUrl ??= 'http://dalalah.co/api';
   }
 
   final Dio _dio;
