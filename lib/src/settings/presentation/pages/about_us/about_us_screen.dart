215bdd41ab5fa00e76773cbf6ba2c17974402ab4
harbey127 <harbey127@gmail.com>
2024-05-05 11:28:32 +0300

    Review Notes

--- lib/src/settings/presentation/pages/about_us/about_us_screen.dart
+++ lib/src/settings/presentation/pages/about_us/about_us_screen.dart
@@ -1,4 +1,3 @@
-import 'package:dalalah/core/exceptions/extensions.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_html/flutter_html.dart';
 
@@ -6,20 +5,16 @@ import '../../../domain/entities/about_us.dart';
 
 class AboutUsScreen extends StatelessWidget {
   final AboutUs data;
+
   const AboutUsScreen({Key? key, required this.data}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    // \r => break line
     return SingleChildScrollView(
       padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 24.0),
       child: Html(
         data: data.description ?? '',
-        style: {
-          'body': Style(
-            fontSize: FontSize(16.0),
-            color: const Color(0xff000000),
-          ),
-        },
       ),
     );
   }
