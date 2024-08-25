8b08cea70558b51f016c6b6fc3b5549cc3d74e31
Yehya <yehyaelzaeem7@gmail.com>
2024-08-25 15:25:03 +0300

    push

---
+++ lib/src/real_estate/data/models/add_rate_developer_params.g.dart
@@ -0,0 +1,21 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'add_rate_developer_params.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+AddRateDeveloperParams _$AddRateDeveloperParamsFromJson(
+        Map<String, dynamic> json) =>
+    AddRateDeveloperParams(
+      propDeveloperId: json['prop_developer_id'] as int?,
+      rate: json['rate'] as int?,
+    );
+
+Map<String, dynamic> _$AddRateDeveloperParamsToJson(
+        AddRateDeveloperParams instance) =>
+    <String, dynamic>{
+      'prop_developer_id': instance.propDeveloperId,
+      'rate': instance.rate,
+    };
